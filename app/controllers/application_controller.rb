e1b42879c3c0475cd1944f21615e4220cb07dff9
John Beynon <jbeynon@salesforce.com>
2020-07-15 14:47:21 +0100

    Different layouts for auth and no auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
   include ApplicationHelper
   before_action :require_login
+
+  layout :determine_layout
+
+  def determine_layout
+    if logged_in?
+      'authed'
+    else
+      'unauthed'
+    end
+  end
 end
